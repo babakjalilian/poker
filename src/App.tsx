0ab102a9daa9dfbaf2018d8cd032d2d4d8a74340
sgh110110110 <babakjalilian1990@gmail.com>
2023-04-01 23:27:16 +0430

    initialize the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+import "./App.css";
+
+
+export const App: React.FC = observer(() => {
+    return (
+        <div className="bg flex-in-col">
+            poker-final-project works!
+        </div>
+    )
+});
