b835ce943d0179453d92095c0bb947afdf5208dd
Babak <babakjalilian1990@gmail.com>
2023-04-05 16:01:26 +0330

    setting layout implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import { observer } from 'mobx-react';
-import React from 'react';
+import { observer } from "mobx-react";
+import React from "react";
 import "./App.css";
-import Card from './components/Card';
-import Game from './components/Game';
+import Game from "./components/Game";
 import Link from "./components/Link";
 import Menu from "./components/Menu";
-import Switch from './components/Switch';
+import Settings from "./components/Setting";
+import Switch from "./components/Switch";
 import store from "./Store";
 
-
 export const App: React.FC = observer(() => {
-    return (
-        <div className="game-background">
-            {
-                store.currentPage !== "Menu" &&
-                <div className="backButton_container">
-                    <Link text="⬅ back to menu" page="Menu" />
-                </div>
-            }
-            {/* <Card value={'10'} suit={"♠"} isFaded={true} isHidden={false} /> */}
-            <Switch activePage={store.currentPage} >
-                <Menu name="Menu" />
-                <Game name="Game" />
-            </Switch>
+  return (
+    <div className="game-background">
+      {store.currentPage !== "Menu" && (
+        <div className="backButton_container">
+          <Link text="⬅ back to menu" page="Menu" />
         </div>
-    )
+      )}
+      {/* <Card value={'10'} suit={"♠"} isFaded={true} isHidden={false} /> */}
+      <Switch activePage={store.currentPage}>
+        <Menu name="Menu" />
+        <Game name="Game" />
+        <Settings name="Settings" />
+      </Switch>
+    </div>
+  );
 });
