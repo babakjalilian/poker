0414601a17b7df8441c512131553d965d1089805
sgh110110110 <babakjalilian1990@gmail.com>
2023-04-07 13:33:12 +0330

    fix resize screen issues

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -21,22 +21,22 @@ const Game: React.FC<GameProps> = observer(() => {
             <div className="poker-container">
                 <div className="poker-table"></div>
                 <div className="outside-ring"></div>
-                <div className="inside-ring"></div>
-                <div className="card-container card-container-topmid">
+                {/* <div className="inside-ring"></div> */}
+                <div className="card-container card-container-topmid player-2">
                     <Player playerId={2} />
                 </div>
-                <div className="card-container card-container-midleft">
+                <div className="card-container card-container-midleft player-1">
                     <Player playerId={1} />
                 </div>
 
-                <div>
+                <div className="game-table">
                     <GameTable />
                 </div>
 
-                <div className="card-container card-container-midright">
+                <div className="card-container card-container-midright player-3">
                     <Player playerId={3} />
                 </div>
-                <div className="card-container card-container-botmid">
+                <div className="card-container card-container-botmid player-0">
                     <Player playerId={0} />
                 </div>
             </div>
