5c64edcbb750112afd945245393460c66c1cfc76
sgh110110110 <babakjalilian1990@gmail.com>
2023-04-27 00:11:21 +0330

    fix stories font style

---
+++ src/stories/Card.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Card from '../components/game/card/Card';
+
+const meta: Meta<typeof Card> = {
+  title: 'Example/Card',
+  component: Card,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+            <div style={{ fontFamily: 'Arial, Helvetica, sans-serif' }}>
+                <Story />
+            </div>
+    )
+  ]
+};
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const TwoOfSpades: Story = {
+  // More on args: https://storybook.js.org/docs/react/writing-stories/args
+  args: {
+    value: '2',
+    suit: '♠'
+  }
+};
+export const TwoOfClubs: Story = {
+  args: {
+    value: '2',
+    suit: '♣'
+  }
+};
+export const TwoOfHearts: Story = {
+  args: {
+    value: '2',
+    suit: '♥'
+  }
+};
+export const TwoOfDiamonds: Story = {
+  args: {
+    value: '2',
+    suit: '♦'
+  }
+};
+export const Faded: Story = {
+  args: {
+    value: '2',
+    suit: '♥',
+    isFaded: true
+  }
+};
+export const Hidden: Story = {
+  args: {
+    value: '2',
+    suit: '♠',
+    isHidden: true
+  }
+};
--- src/stories/Player.stories.tsx
+++ src/stories/Player.stories.tsx
@@ -3,105 +3,104 @@ import Player from '../components/game/player/Player';
 import Card from '../stores/Card';
 
 const meta: Meta<typeof Player> = {
-    title: 'Example/Player',
-    component: Player,
-    tags: ['autodocs'],
-    decorators: [
-        (Story) => (
-            <div style={{ position: 'absolute' }}>
+  title: 'Example/Player',
+  component: Player,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+            <div style={{ position: 'absolute', fontFamily: 'Arial, Helvetica, sans-serif' }}>
                 <Story />
             </div>
-        ),
-    ],
+    )
+  ]
 };
-
 
 export default meta;
 type Story = StoryObj<typeof Player>;
 
-const cards = [new Card("hearts", "ace"), new Card("spades", "king"),]
+const cards = [new Card('hearts', 'ace'), new Card('spades', 'king')];
 
 export const BigBlindPlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: false,
-        isActivePlayer: false,
-        isBigBlindPlayer: true,
-        isSmallBlindPlayer: false,
-        isWinner: false,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: false,
+    isActivePlayer: false,
+    isBigBlindPlayer: true,
+    isSmallBlindPlayer: false,
+    isWinner: false,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
 export const SmallBlindPlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: false,
-        isActivePlayer: false,
-        isBigBlindPlayer: false,
-        isSmallBlindPlayer: true,
-        isWinner: false,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: false,
+    isActivePlayer: false,
+    isBigBlindPlayer: false,
+    isSmallBlindPlayer: true,
+    isWinner: false,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
 export const ActivePlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: false,
-        isActivePlayer: true,
-        isBigBlindPlayer: false,
-        isSmallBlindPlayer: false,
-        isWinner: false,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: false,
+    isActivePlayer: true,
+    isBigBlindPlayer: false,
+    isSmallBlindPlayer: false,
+    isWinner: false,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
 export const NormalPlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: false,
-        isActivePlayer: false,
-        isBigBlindPlayer: false,
-        isSmallBlindPlayer: false,
-        isWinner: false,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: false,
+    isActivePlayer: false,
+    isBigBlindPlayer: false,
+    isSmallBlindPlayer: false,
+    isWinner: false,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
 export const WinnerPlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: false,
-        isActivePlayer: false,
-        isBigBlindPlayer: false,
-        isSmallBlindPlayer: false,
-        isWinner: true,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: false,
+    isActivePlayer: false,
+    isBigBlindPlayer: false,
+    isSmallBlindPlayer: false,
+    isWinner: true,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
 export const FoldedPlayerInfo: Story = {
-    args: {
-        playerId: 0,
-        name: "player-0",
-        hasFolded: true,
-        isActivePlayer: false,
-        isBigBlindPlayer: false,
-        isSmallBlindPlayer: false,
-        isWinner: false,
-        sumOfPersonalBetsInThisRound: 200,
-        moneyLeft: 800,
-        cards: cards
-    },
+  args: {
+    playerId: 0,
+    name: 'player-0',
+    hasFolded: true,
+    isActivePlayer: false,
+    isBigBlindPlayer: false,
+    isSmallBlindPlayer: false,
+    isWinner: false,
+    sumOfPersonalBetsInThisRound: 200,
+    moneyLeft: 800,
+    cards
+  }
 };
